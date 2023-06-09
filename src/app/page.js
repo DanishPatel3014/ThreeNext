e2c59b0228dafd29ab993af4a0ddbd2be8f078c8
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-06-09 05:45:54 -0700

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+"use client";
+import Experience from '@/components/Experience.js';
+import { Canvas } from '@react-three/fiber'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-    <mesh>
-      
-    </mesh>
+   <section className='mainbanner'>
+   <Canvas>
+      <Experience/>
+    </Canvas>
+   </section>
     </>
   )
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+
+export default function Experience() {
+  return (
+    <>
+    <mesh>
+        <sphereGeometry args={[1.5,32.32]} />
+        <meshNormalMaterial/>
+      </mesh>
+    </>
+  )
+}
