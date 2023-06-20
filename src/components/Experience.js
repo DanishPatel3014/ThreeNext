adcf1e815213cfe72c2f3d2a8f2ec5c317198ab1
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-06-20 06:38:20 -0700

    updates

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,11 +3,16 @@ import { Html, OrbitControls, TransformControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import { useRef } from "react";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 
 // extend({OrbitControls})
 
 export default function Experience() {
+
+  const model = useLoader(GLTFLoader,'../../hamburger.glb')
+  
 
   const {position} = useControls({
     position:
@@ -36,20 +41,21 @@ export default function Experience() {
     <OrbitControls makeDefault />
     <directionalLight position={[1,2,3]} intensity={1} />
     <ambientLight intensity={0.5}/>
-    <mesh position-x={position}>
+    {/* <mesh position-x={position}>
         <sphereGeometry  />
         <meshStandardMaterial color={'#f3992f'}/>
       </mesh>
       <mesh ref={cubeRef} rotateY={Math.PI * 0.25} position-x={2} scale={1.5}>
         <boxGeometry scale={1.5} />
         <meshStandardMaterial color={'#5bcefa'} />
-        {/* <Html>Danish</Html> */}
-      </mesh>
+        <Html>Danish</Html>
+      </mesh> */}
       {/* <TransformControls object={cubeRef} /> */}
       <mesh  position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
         <planeGeometry/>
         <meshStandardMaterial color={'#84ff76'}/>
       </mesh>
+      <primitive object={model.scene} scale={'0.35'} />
     </>
   )
 }
